f2a84a2a45e97124a79719d264bd709f1d59a361
saad ahmed <saad ahmed>
2018-11-16 12:04:41 -0500

    added new components and added app icon and styling changes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import { Container, Content, H1 } from 'native-base';
+import firebase from 'firebase';
+import { Container, Content, Button, Text, H1 } from 'native-base';
 
 export default class Dashboard extends Component {
+    onButtonPress(){
+        console.log(firebase.auth)
+    }
     render() {
         return (
             <Container>
                 <Content>
                     <H1>Dashboard</H1>
+                    <Button>
+                        <Text> Press this</Text>
+                    </Button>
                 </Content>
             </Container>
         );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import firebase from 'firebase'; 
+import { TouchableOpacity } from 'react-native';
 import { Button, Text, Container, Content, Header, Footer, Form, Item, Input, H1 } from 'native-base';
 
 export default class Login extends Component {
@@ -7,34 +8,56 @@ export default class Login extends Component {
         super(props);
         this.state = {email: '', password: '', error: ''};
       }
+
+    static navigationOptions = {
+        header: null,     
+    };
     onLoginPress(){
         console.log(this.state.email)
         console.log(this.state.password)
+        //this.props.navigation.navigate('DashBoard')
 
         firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
             .then(()=>this.props.navigation.navigate('DashBoard'))
+                .catch((e)=> this.setState( { error: e } ))
+    }
+    onForgotPress(){
+        console.log('poop')
+    }
+
+    showError(){
+        if(this.state.error){
+            return(
+                <Text style={{marginTop:10, color:'red'}}> * {this.state.error.message}</Text>
+            )
+        }
     }
 
     render(){
         return (
-            <Container>
-                <Content>
+            <Container style={{justifyContent:'space-between', alignItems:'center'}}>
+                <Form style={{alignItems:'center'}}>
+                <H1 style={{marginTop:30}}>Login</H1>
+                    <Item>
+                        <Input placeholder="Username" onChangeText={(text) => this.setState({email: text})}/>
+                    </Item>
                     
-                    <Form style={{alignItems:'center'}}>
-                    <H1 style={{margin:10}}>Login</H1>
-                        <Item>
-                            <Input placeholder="Username" onChangeText={(text) => this.setState({email: text})}/>
-                        </Item>
-                        <Item last>
-                            <Input secureTextEntry placeholder="Password" onChangeText={(text) => this.setState({password: text})}/>
-                        </Item>
-                        <Button block success style={{margin:15}} onPress={this.onLoginPress.bind(this)}>
-                            <Text>Login</Text>
-                        </Button>
-                    </Form>
+                    <Item last>
+                        <Input secureTextEntry placeholder="Password" onChangeText={(text) => this.setState({password: text})}/>
+                    </Item>
+                    
+                    {this.showError()}
 
-                </Content>
-                <Footer />
+                    <Button block style={{margin:15}} onPress={this.onLoginPress.bind(this)}>
+                        <Text>Login</Text>
+                    </Button>
+                    <TouchableOpacity onPress={this.onForgotPress.bind(this)}>
+                      <Text>Forgot your password? Click Here </Text>
+                    </TouchableOpacity>
+                </Form>
+                <TouchableOpacity>
+                   <Text style={{margin:15}}>Don't have an account? Click Here</Text>               
+                </TouchableOpacity>
           </Container>
         );
     }
